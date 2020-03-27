633f0a26c64a8cccdeaddbdd4cc205d48971687a
LUIS ALBERTO VISCARRA HOLGUIN <18170245@itculiacan.edu.mx>
2020-03-26 19:31:34 -0700

    prueba1

--- WindowsFormsProyectoRestaurante/Program.cs
+++ WindowsFormsProyectoRestaurante/Program.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsProyectoRestaurante
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.EnableVisualStyles();
